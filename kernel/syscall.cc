05100305eaa056bd4a7d9011a4a33e1cc0efb762
Grace Shiau <sgrace@cs.utexas.edu>
2014-12-08 05:13:37 -0600

    Committing before pulling

--- kernel/syscall.cc
+++ kernel/syscall.cc
@@ -107,7 +107,7 @@ extern "C" long syscallHandler(uint32_t* context, long num, long a0, long a1) {
         {
             File* f = FileSystem::rootfs->rootdir->lookupFile((char*) a0);
             if (f == nullptr) return ERR_NOT_FOUND;
-            bool access = true; //Process::current->userPermissions->Access((const char*)a0, 0);
+            bool access = Process::current->userPermissions->Access((const char*)a0, 0);
             if (!access) return ERR_ACCESS_DENIED;
             else return Process::current->resources->open(f);
         }
